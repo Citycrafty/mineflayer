10cdeea15a45824eb2814ba55d68f77d64128785
Citycrafty <105089147+Citycrafty@users.noreply.github.com>
2022-06-25 15:20:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[
   console.error('Your node version is currently', process.versions.node)
   console.error('Please update it to a version >= 14.x.x from https://nodejs.org/')
   process.exit(1)
+Host: citycrafty.aternos.me
+Port: 63290
+Name: city
 }
 
 module.exports = require('./lib/loader.js')
